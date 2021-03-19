004e965d2f67d84fde9b2e96e3df3be8df3a91e5
AbduvorisAnvarov <72465393+AbduvorisAnvarov@users.noreply.github.com>
2021-03-19 13:41:05 -0700

    athletes

---
+++ data/athletes-type.js
@@ -0,0 +1,124 @@
+{
+	"title": "Athletes Sorted by Card",
+	"athletes": [
+		{
+            "Name": "Christiano Ronaldo",
+            "Sport": "Soccer",
+            "Height": "6'2",
+            "Age": "36",
+			"Image": "ronaldo.jpg"
+        },
+
+        {
+            "Name": "Leonel Messi",
+            "Sport": "Soccer",
+            "Height": "5'7",
+            "Age": "33",
+			"Image": "messi.jpg"
+        },
+
+		{
+            "Name": "Khabib Nurmagomedov",
+            "Sport": "MMA",
+            "Height": "5'10",
+            "Age": "32",
+			"Image": "khabib.jpg"
+        },
+
+        {
+            "Name": "Muhammad Ali",
+            "Sport": "Boxing",
+            "Height": "6'3",
+            "Age": "1986-2016",
+			"Image": "ali.jpg"
+        },
+
+        {
+            "Name": "Tom Brady",
+            "Sport": "Football",
+            "Height": "6'4",
+            "Age": "43",
+			"Image": "brady.jpg"
+        },
+
+        {
+            "Name": "Israel Adesanya",
+            "Sport": "MMA",
+            "Height": "6'2",
+            "Age": "31",
+			"Image": "adesanya.jpg"
+        },
+
+        {
+            "Name": "Kobe Bryant",
+            "Sport": "Basketball",
+            "Height": "6'6",
+            "Age": "1978-2020",
+			"Image": "kobe.jpg"
+        },
+
+        {
+            "Name": "Gary Payton",
+            "Sport": "Basketball",
+            "Height": "6'4",
+            "Age": "52",
+			"Image": "payton.jpg"
+        },
+
+        {
+            "Name": "Muhammad Salah",
+            "Sport": "Soccer",
+            "Height": "5'9",
+            "Age": "28",
+			"Image": "salah.jpg"
+        },
+
+        {
+            "Name": "Mike Tyson",
+            "Sport": "Boxing",
+            "Height": "5'10",
+            "Age": "54",
+			"Image": "tyson.jpg"
+        },
+
+        {
+            "Name": "Yoan Moncada",
+            "Sport": "Baseball",
+            "Height": "6'2",
+            "Age": "25",
+			"Image": "moncada.jpeg"
+        },
+
+        {
+            "Name": "Russell Wilson",
+            "Sport": "Football",
+            "Height": "5'11",
+            "Age": "32",
+			"Image": "wilson.jpg"
+        },
+
+        {
+            "Name": "Jorge Masvidal",
+            "Sport": "MMA",
+            "Height": "5'11",
+            "Age": "36",
+			"Image": "masvidal.jpg"
+        },
+
+        {
+            "Name": "Lebron James",
+            "Sport": "Basketball",
+            "Height": "6'9",
+            "Age": "36",
+			"Image": "lebron.jpg"
+        },
+
+        {
+            "Name": "Michael Jordan",
+            "Sport": "Basketball",
+            "Height": "6'6",
+            "Age": "58",
+			"Image": "jordan.jpg"
+        }
+	]
+}
